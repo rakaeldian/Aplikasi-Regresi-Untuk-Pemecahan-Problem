6fe1e1ecd5943e9687b27283c74980189a0956c8
rakaeldian <132545309+rakaeldian@users.noreply.github.com>
2024-06-09 01:08:33 +0700

    Update PangkatSederhana.py

--- Aplikasi_Regresi/Aplikasi_Regresi/PangkatSederhana.py
+++ Aplikasi_Regresi/Aplikasi_Regresi/PangkatSederhana.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 
-data_url = r"C:\Users\Public\python-2\student_performance.csv"  # Ganti dengan path yang sesuai
+data_url = r"C:\Users\Public\Download\student_performance.csv"
 data = pd.read_csv(data_url)
 
 data = data[['Hours Studied', 'Sample Question Papers Practiced', 'Performance Index']]
